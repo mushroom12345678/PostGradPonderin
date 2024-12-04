d198af6394af7567fd3d792d23d6c0c7846f5da4
jdetweiler1 <j.detweiler@ufl.edu>
2024-12-03 20:57:01 -0500

    eddited merge sort and added time

--- MergeSort.h
+++ MergeSort.h
@@ -3,13 +3,14 @@
 //
 #include <iostream>
 #include <vector>
+#include "Counties.h"
 
 using namespace std;
 //used class resourses, slides/lectures, to form code, Jack Detweiler 11/22
 class MergeSort {
 public:
-    void merge(vector<int>& list, int left, int mid, int right);
-    void mergeSort(vector<int>& list, int left, int right);
+    void merge(vector<Counties>& list, int left, int mid, int right);
+    void mergeSort(vector<Counties>& list, int left, int right);
 };
 #ifndef MERGESORT_MERGESORT_H
 #define MERGESORT_MERGESORT_H
